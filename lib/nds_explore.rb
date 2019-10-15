3506c2b8f429d7d2bf0cfe2db3f9391b7f0b4d8c
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-15 02:54:10 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,15 @@
+require 'pry'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nil
 end
 
 def print_first_directors_movie_titles
+    directors_database.each {|word| print word,'\n'}
+    binding.pry
+      
 end
